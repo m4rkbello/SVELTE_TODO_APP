edb82b5f8250937ad020821e90352e265dda211b
m4rkbello <markbello10041999@gmail.com>
2025-07-27 22:48:33 +0800

    ADDED

---
+++ src/components/TodoForm.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import type { Todo } from "../types/todo";
+
+  const dispatch = createEventDispatcher<{
+    add: Todo;
+  }>();
+
+  //initializer choi
+  let text = "";
+  let priority: Todo["priority"] = "medium";
+  let category = "";
+  let showAdvanced = false;
+</script>
+
+<form class="todo-form">
+  <div class="input-group">
+    <div class="text-input-wrapper">
+      <input type="text" bind:value{text} onkeydown="" class="text-input" />
+    </div>
+  </div>
+</form>
